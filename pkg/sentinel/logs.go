022dd67d281f1d3eddf90b4b0523355c674b2702
Niels Hofmans <niels@ironpeak.be>
2025-02-21 13:55:51 +0100

    fix(logs): fix chunked ingest

--- pkg/sentinel/logs.go
+++ pkg/sentinel/logs.go
@@ -8,39 +8,66 @@ import (
 )
 
 const (
-	maxChunkSize = 1 * 1024 * 1024 // 1MB
+	maxChunkSize = 1000 * 1000 // 1MB
 )
 
 // estimateSize estimates the size of a log batch in bytes
-func estimateSize(logs []map[string]string) int {
-	data, _ := json.Marshal(logs)
-	return len(data)
+func (s *Sentinel) estimateSize(logs []map[string]string) (int, error) {
+	data, err := json.Marshal(logs)
+	if err != nil {
+		return 0, err
+	}
+	return len(data), nil
 }
 
-func chunkLogs(slice []map[string]string) [][]map[string]string {
+func (s *Sentinel) chunkLogs(slice []map[string]string) ([][]map[string]string, error) {
 	var chunks [][]map[string]string
 	var currentChunk []map[string]string
+	var currentSize int
+
 	for _, logEntry := range slice {
-		if estimateSize(append(currentChunk, logEntry)) > maxChunkSize && len(currentChunk) > 0 {
+		// Estimate size with the new log added
+		logSize, err := s.estimateSize([]map[string]string{logEntry}) // Estimate single log size
+		if err != nil {
+			return nil, fmt.Errorf("error estimating log size: %v", err)
+		}
+
+		// If adding this log exceeds maxChunkSize, save the current chunk
+		if currentSize+logSize > maxChunkSize {
+			if len(currentChunk) == 0 {
+				// Handle case where a single log is larger than the max chunk size
+				return nil, fmt.Errorf("single log exceeds max chunk size")
+			}
 			chunks = append(chunks, currentChunk)
 			currentChunk = nil
+			currentSize = 0
 		}
+
+		// Add log to the current chunk
 		currentChunk = append(currentChunk, logEntry)
+		currentSize += logSize
 	}
 
+	// Append any remaining logs
 	if len(currentChunk) > 0 {
 		chunks = append(chunks, currentChunk)
 	}
 
-	return chunks
+	return chunks, nil
 }
 
 func (s *Sentinel) SendLogs(ctx context.Context, l *logrus.Logger, endpoint, ruleID, streamName string, logs []map[string]string) error {
 	logger := l.WithField("module", "sentinel_logs")
 
-	logger.WithField("stream_name", streamName).WithField("total", len(logs)).Info("shipping logs")
+	logger.WithField("stream_name", streamName).WithField("total", len(logs)).Info("chunking logs")
 
-	chunkedLogs := chunkLogs(logs)
+	chunkedLogs, err := s.chunkLogs(logs)
+	if err != nil {
+		return fmt.Errorf("failed to chunk logs: %v", err)
+	}
+
+	logger.WithField("chunked_logs", len(chunkedLogs)).Info("sending chunked logs")
+
 	for i, logsChunk := range chunkedLogs {
 		l.WithField("progress", fmt.Sprintf("%d/%d", i+1, len(chunkedLogs))).Debug("ingesting log chunks")
 
